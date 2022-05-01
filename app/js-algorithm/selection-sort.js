3a07677e56f02f8de54cb719ad77971152f0cdbd
kei-yagi <kei.yagi@gmail.com>
2022-05-01 22:50:18 +0900

    selection-sort

---
+++ app/js-algorithm/selection-sort.js
@@ -0,0 +1,15 @@
+// https://www.30secondsofcode.org/js/s/selection-sort
+
+const selectionSort = (arr) => {
+  const a = [...arr];
+  for (let i = 0; i < a.length; i++) {
+    const min = a
+      .slice(i + 1)
+      .reduce((acc, val, j) => (val < a[acc] ? j + i + 1 : acc), i);
+
+    if (min !== i) [a[i], a[min]] = [a[min], a[i]];
+  }
+  return a;
+};
+
+console.log(selectionSort([5, 1, 4, 2, 3]));
