96b7a4b22ed9da57e06dbaece453001555691ffe
kei-yagi <kei.yagi@gmail.com>
2022-05-03 23:58:54 +0900

    primes

--- app/js-algorithm/count-substrings.js
+++ app/js-algorithm/count-substrings.js
@@ -1,13 +1,14 @@
 // https://www.30secondsofcode.org/js/s/count-substrings
 
 const countSubstrings = (str, searchValue) => {
-  let count = 0, i = 0
+  let count = 0,
+    i = 0;
   while (true) {
-    const r = str.indexOf(searchValue, i)
-    if (r !== -1) [count, i] = [count + 1, r + 1]
-    else return count
+    const r = str.indexOf(searchValue, i);
+    if (r !== -1) [count, i] = [count + 1, r + 1];
+    else return count;
   }
-}
+};
 
-console.log(countSubstrings('tiktok tok tok tik tok tik', 'tik'))
-console.log(countSubstrings('tutut tut tut', 'tut'))
+console.log(countSubstrings("tiktok tok tok tik tok tik", "tik"));
+console.log(countSubstrings("tutut tut tut", "tut"));
