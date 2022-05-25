00c381bcb53106a8b8c535789d35f0b296eccd59
kei-yagi <kei.yagi@gmail.com>
2022-05-25 23:38:03 +0900

    hash-node

---
+++ app/node/hash-node.js
@@ -0,0 +1,15 @@
+// https://www.30secondsofcode.org/js/s/hash-node
+
+const crypto = require('crypto')
+
+const hashNode = val =>
+  new Promise(resolve =>
+    setTimeout(
+      () => resolve(crypto.createHash('sha256').update(val).digest('hex')),
+      0
+    )
+  )
+
+hashNode(JSON.stringify({ a: 'a', b: [1, 2, 3, 4], foo: { c: 'bar' }})).then(
+  console.log
+)
