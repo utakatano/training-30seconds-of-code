e6ca85a4e06cb2e64c2f48c5e9a3fa18fa7fc247
kei-yagi <kei.yagi@gmail.com>
2022-05-27 23:58:41 +0900

    is-duplex-stream-1

---
+++ app/node/is-duplex-stream.js
@@ -0,0 +1,11 @@
+// https://www.30secondsofcode.org/js/s/is-duplex-stream
+
+const isDuplexStream = val =>
+  val !== null &&
+  typeof val === 'object' &&
+  typeof val.pipe === 'function' &&
+  typeof val._read === 'function' &&
+  typeof val._readableState === 'object' &&
+  typeof val._write === 'function' &&
+  typeof val._writableState === 'object'
+
