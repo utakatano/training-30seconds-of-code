8c21594ce6352d7a096d20cb6a7d5c7ff86138e6
kei-yagi <kei.yagi@gmail.com>
2022-06-14 23:52:51 +0900

    javascript-sleep-2

--- app/js-date/javascript-sleep.js
+++ app/js-date/javascript-sleep.js
@@ -1,8 +1,33 @@
 // https://www.30secondsofcode.org/articles/s/javascript-sleep
 
-const printNums = () => {
+// https://www.30secondsofcode.org/articles/s/javascript-sleep#set-timeout
+// const printNums = () => {
+//   console.log(1)
+//   setTimeout(() => console.log(2), 500)
+//   console.log(3)
+// }
+
+// https://www.30secondsofcode.org/articles/s/javascript-sleep#synchronous-version
+// const sleepSync = (ms) => {
+//   const end = new Date().getTime() + ms
+//   while (new Date().getTime() < end) { /* do nothing */ }
+// }
+
+// const printNums = () => {
+//   console.log(1)
+//   sleepSync(500)
+//   console.log(2)
+//   console.log(3)
+// }
+
+// https://www.30secondsofcode.org/articles/s/javascript-sleep#asynchronous-version
+const sleep = (ms) =>
+  new Promise(resolve => setTimeout(resolve, ms))
+
+const printNums = async () => {
   console.log(1)
-  setTimeout(() => console.log(2), 500)
+  await sleep(500)
+  console.log(2)
   console.log(3)
 }
 
