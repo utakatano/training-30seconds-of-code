3db15220a69e292ffb0470587fb4eae383328db6
kei-yagi <kei.yagi@gmail.com>
2022-06-21 23:57:36 +0900

    add-week-days

---
+++ app/js-date/add-week-days.js
@@ -0,0 +1,12 @@
+// https://www.30secondsofcode.org/js/s/add-week-days
+
+const addWeekDays = (startDate, count) =>
+  Array.from({ length: count }).reduce(date => {
+    date = new Date(date.setDate(date.getDate() + 1))
+    if (date.getDay() % 6 === 0)
+      date = new Date(date.setDate(date.getDate() + (date.getDay() / 6 + 1)))
+    return date
+  }, startDate)
+
+console.log(addWeekDays(new Date('Oct 09, 2020'), 5))
+console.log(addWeekDays(new Date('Oct 12, 2020'), 5))
