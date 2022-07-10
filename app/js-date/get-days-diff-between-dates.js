68a6cbe53f3cc5d893e96533ae87fcce7491409b
kei-yagi <kei.yagi@gmail.com>
2022-07-10 23:58:12 +0900

    get-days-diff-between-dates-console

--- app/js-date/get-days-diff-between-dates.js
+++ app/js-date/get-days-diff-between-dates.js
@@ -3,3 +3,4 @@
 const getDaysDiffBetweenDates = (dateInitial, dateFinal) =>
   (dateFinal - dateInitial) / (1000 * 3600 * 24)
 
+console.log(getDaysDiffBetweenDates(new Date('2017-12-13'), new Date('2017-12-22')))
